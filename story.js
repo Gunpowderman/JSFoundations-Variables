59e3092c1c76ec0589525f08ad8b2937270a617b
Gunpowderman <adel.baroody@gmail.com>
2021-02-01 19:12:13 +0300

    Task 3 variables

---
+++ story.js
@@ -0,0 +1,5 @@
+let Name = "Adel";
+let Age = 36;
+let Hobby = "Cooking";
+
+console.log (`Mr./Mrs. ${Name}, in 33 years, you will be ${Age+33} years old. By then you'll have decades of ${Hobby} practice.` );
